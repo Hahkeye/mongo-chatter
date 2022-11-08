b7eeb9cac317b438044324cc0c684a821cd94b57
Hunter <realisvenator@gmail.com>
2022-11-07 18:27:03 -0600

    woops

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Handlebars.registerPartial('postP','{{prefix}}');
-mongo.test;
+// mongo.test;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
